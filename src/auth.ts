bd93311a53a6530ca5aca9e1fe24f5cb2d4e6f08
badadarr <badar.maulana.techno@gmail.com>
2026-08-21 16:11:02 +0700

    feat: implement NextAuth configuration and credentials provider for JWT-based authentication

--- src/auth.ts
+++ src/auth.ts
@@ -6,6 +6,8 @@ import { authConfig } from "@/auth.config";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   ...authConfig,
+  trustHost: true,
+  secret: process.env.AUTH_SECRET || process.env.NEXTAUTH_SECRET,
   session: { strategy: "jwt" },
   providers: [
     Credentials({
